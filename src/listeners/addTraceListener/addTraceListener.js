70ba181866ca0f1231a0923f3c8e09ee46131167
fongelias <fongelias@gmail.com>
2017-12-16 21:53:34 -0500

    added integration with sendTelemetry as well as doc comments

--- src/listeners/addTraceListener/addTraceListener.js
+++ src/listeners/addTraceListener/addTraceListener.js
@@ -2,17 +2,23 @@ import { EventEmitter } from 'events';
 
 
 import { listeners } from '../../config';
+import { sendTelemetry } from '../../requests';
 
-
-export function addTraceListener(emitter, url, apiKey, appName) {
-	console.log('addTraceListener');
+/** 
+function to add a listener for trace events that sends telemetry
+@param {EventEmitter} emitter - the event emitter used by the client to manage loudmouth related events
+@param {URL} url - a url object
+@param {string} appName - the name of the application sending telemetry
+@param {string} apiKey - the api key used to access the url
+*/
+export function addTraceListener(emitter, url, appName, apiKey) {
+	//console.log('addTraceListener');
 	//Type Check
 	if(!(emitter instanceof EventEmitter)) {
 		throw new Error('addTraceListener: argument is not an EventEmitter');
 	}
 
 	emitter.on(listeners.events.traceListener, () => {
-		console.log('hey');
-		//https.request() function here
+		sendTelemetry(url, appName, apiKey);
 	})
 }
